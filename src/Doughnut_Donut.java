2cf72fb5965d5204c3cbd9316413a9b36bf2b198
runxue00 <league@iMac.attlocal.net>
2018-04-14 15:57:54 -0700

    Class
    MehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMehMeh
    MehMehMehMehMehMeh

---
+++ src/Doughnut_Donut.java
@@ -0,0 +1,28 @@
+
+public class Doughnut_Donut {
+private String Color;
+private String Brand;
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+Doughnut_Donut(String Color, String Brand){
+	this.Color=Color;
+	this.Brand=Brand;
+}
+public void setColor(String Color) {
+	this.Color=Color;
+}
+public String getColor() {
+	return this.Color;
+}
+public void setBrand(String Brand) {
+	this.Brand=Brand;
+}
+public String getBrand() {
+	return this.Brand;
+}
+
+
+
+
+}
